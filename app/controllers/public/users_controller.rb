9a7ac40e20bbaf7862237b54a058940a4bc78f19
Na2Key2261 <natsuki.97285@gmail.com>
2024-02-25 09:46:47 +0000

    fix layout

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -10,6 +10,23 @@ class Public::UsersController < ApplicationController
 
   # 学習時間の合計
   @total_learning_hours = @user.posts.sum(:learning_hour)
+
+# 日付を指定して学習時間の合計と学習項目を取得
+  if params[:search_date].present?
+    search_date = Date.parse(params[:search_date])
+    posts_on_date = @user.posts.where(created_at: search_date.all_day)
+    @total_learning_hours_on_date = posts_on_date.sum(:learning_hour)
+    @learning_items_on_date = posts_on_date.group(:learning_item).sum(:learning_hour)
+  end  
+  # 当日の学習時間の合計と学習項目
+    today_posts = @user.posts.where(created_at: Date.today.all_day)
+    @total_today_learning_hours = today_posts.sum(:learning_hour)
+    @learning_items_today = today_posts.group(:learning_item).sum(:learning_hour)
+
+    # 前日の学習時間の合計と学習項目
+    yesterday_posts = @user.posts.where(created_at: 1.day.ago.all_day)
+    @total_yesterday_learning_hours = yesterday_posts.sum(:learning_hour)
+    @learning_items_yesterday = yesterday_posts.group(:learning_item).sum(:learning_hour)
   
 
   # 学習項目別の学習時間の合計
@@ -21,10 +38,10 @@ class Public::UsersController < ApplicationController
   weekly_learning_hours = @user.posts.where(created_at: start_date..end_date).group("DATE(created_at)").sum(:learning_hour)
 
   @weekly_learning_hours = []
-  (start_date..Date.today).each do |date|
-    formatted_date = date.strftime("%Y-%m-%d")
-    @weekly_learning_hours << { date: formatted_date, hours: weekly_learning_hours[formatted_date] || 0 } if @weekly_learning_hours.length < 7 && weekly_learning_hours[formatted_date]
-  
+  (start_date..Date.today).reverse_each do |date|
+  formatted_date = date.strftime("%Y-%m-%d")
+  @weekly_learning_hours << { date: formatted_date, hours: weekly_learning_hours[formatted_date] || 0 } if @weekly_learning_hours.length < 7 && weekly_learning_hours[formatted_date]
+
   end
   # 過去一週間の学習時間の合計
   @total_weekly_hours = @weekly_learning_hours.sum { |data| data[:hours] }
