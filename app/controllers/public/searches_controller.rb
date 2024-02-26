bd3c82cc19dc5a19f12bff7cab6563e1108da0ec
Na2Key2261 <natsuki.97285@gmail.com>
2024-02-26 10:50:59 +0000

    fix js

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,16 +3,16 @@ class Public::SearchesController < ApplicationController
   
 
   def search
-  @method = params[:search_type]
-
-  return if @method.blank?
-
-  if @method == 'learning_item'
-    @posts = Post.where(learning_item: params[:learning_item]).order(created_at: :desc)
-  elsif @method == 'learning_content'
-    @posts = Post.where('learning_content LIKE ?', "%#{params[:learning_content]}%").order(created_at: :desc)
+    @method = params[:search_type]
+  
+    return if @method.blank?
+  
+    if @method == 'learning_item'
+      @posts = Post.where(learning_item: params[:learning_item]).order(created_at: :desc)
+    elsif @method == 'learning_content'
+      @posts = Post.where('learning_content LIKE ?', "%#{params[:learning_content]}%").order(created_at: :desc)
+    end
   end
-end
 
 
 end
