40ef6f0418bb3e451bfdd8c8ce4da0ca90cd6b40
Na2Key2261 <natsuki.97285@gmail.com>
2024-02-27 11:19:04 +0000

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -16,10 +16,10 @@ import "../stylesheets/application";
 
 
 Rails.start()
-Turbolinks.start()
+//Turbolinks.start()
 ActiveStorage.start()
 
-document.addEventListener("turbolinks:load", () => {
-  Turbolinks.clearCache();
-});
+//document.addEventListener("turbolinks:load", () => {
+  //Turbolinks.clearCache();
+//});
 
